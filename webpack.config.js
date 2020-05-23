e3035b524c0eeb1ef40c8b13ccd9acb141aae8d5
Thaise Dantas <thaiserpdantas@gmail.com>
2020-05-23 20:48:19 -0300

    :lipstick: Add css

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,9 @@ module.exports = {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public')
+    },
     module: {
         rules: [
             {
@@ -14,10 +17,21 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
+            },
+            {
+                test: /.*\.(gif|png|jpe?g)$/i,
+                use: {
+                    loader: 'file-loader'
+                }
             }
         ]
     },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public')
-    }
 }
