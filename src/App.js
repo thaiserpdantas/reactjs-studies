2a502a90c7cf023e4afd45067365f2eae6fadc9b
Thaise Dantas <thaiserpdantas@gmail.com>
2020-05-23 20:18:16 -0300

    :alembic: Working useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header'
 
 function App() {
 
-    const projects = ['Desenvolvimento de app', 'Front-end Web']
+    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end Web']);
+
+    function handleAddProject() {
+        // projects.push(`Novo projeto ${Date.now()}`);
+
+        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+        console.log(projects);
+    }
 
     return (
         // react fragment
         <>
-            <Header title="Homepage">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                </ul>
-            </Header>
-            <Header title="Projects">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                    <li>Login</li>
-                </ul>
-            </Header>
+            <Header title="Projects" />
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
         </>
     );
 }
