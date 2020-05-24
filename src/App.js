ec75966da0de15148e82a529269d0426a28a4c0d
Thaise Dantas <thaiserpdantas@gmail.com>
2020-05-23 21:35:41 -0300

    :alembic: Connect with backend

--- src/App.js
+++ src/App.js
@@ -5,19 +5,16 @@ import api from './services/api'
 
 function App() {
 
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end Web']);
+    const [projects, setProjects] = useState([]);
 
     useEffect(() => {
         api.get('projects').then(response => {
-            console.log(response)
+            setProjects(response.data)
         });
     }, []);
 
     function handleAddProject() {
-        // projects.push(`Novo projeto ${Date.now()}`);
-
         setProjects([...projects, `Novo projeto ${Date.now()}`]);
-        console.log(projects);
     }
 
     return (
@@ -26,7 +23,7 @@ function App() {
             <Header title="Projects" />
 
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
 
             <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
